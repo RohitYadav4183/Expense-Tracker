f5ff8dbf641e9870df9b6d09714ca399334dc280
RohitYadav4183 <102618406+RohitYadav4183@users.noreply.github.com>
2023-06-22 11:05:01 +0530

    Update connectDb.js

--- config/connectDb.js
+++ config/connectDb.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const colors = require("colors");
 const connectDb = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect("mongodb+srv://Rohit_1009:rohit1009@expense-management-syst.bfynzpc.mongodb.net/",{useUnifiedTopology: true , useNewUrlParser : true});
     console.log(`Server Running On ${mongoose.connection.host}`.bgCyan.white);
   } catch (error) {
     console.log(`${error}`.bgRed);
